7c6a08c08e58ccacf810ab1d49d915e2d48fcafe
Thiago Henrique <thiagonascimento95@hotmail.com>
2018-05-22 22:13:29 -0300

    initial commit

---
+++ src/main/java/com/thiagodev/mailschedulerspring/MailSchedulerSpringApplication.java
@@ -0,0 +1,12 @@
+package com.thiagodev.mailschedulerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSchedulerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailSchedulerSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/thiagodev/mailschedulerspring/MailSchedulerSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thiagodev.mailschedulerspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MailSchedulerSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
